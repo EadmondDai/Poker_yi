3308d517a56293d9fecb9a5a7024b25e092c6d43
kthtes <kthtes@gmail.com>
2016-10-06 16:05:11 -0700

    Memory leak checked.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,14 @@
+#define _CRTDBG_MAP_ALLOC
+#define _CRTDBG_MAP_ALLOC_NEW
+#include <cstdlib>
+#include <crtdbg.h>
+#ifdef _DEBUG
+#ifndef DBG_NEW
+#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__, __LINE__ )
+#define new DBG_NEW
+#endif
+#endif
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -10,14 +21,16 @@ using namespace std;
 
 int main(){
 
-    CardListTest::test();
-    PlayerTest::test();
+//    CardListTest::test();
+//    PlayerTest::test();
 
     // setup rand
     srand(time(nullptr));
 
-    Player p;
-    p.start();
+    Player* p=new Player;
+    p->start();
 
+	delete p;
+	_CrtDumpMemoryLeaks();
     return 0;
 }
